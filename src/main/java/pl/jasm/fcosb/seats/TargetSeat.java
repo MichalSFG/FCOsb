18270f87a4d5b3b81fa3694ccf37ad2fb423a5d3
MichalSFG <mickeyjas80@gmail.com>
2020-12-04 18:09:32 +0100

    entities + views resources

---
+++ src/main/java/pl/jasm/fcosb/seats/TargetSeat.java
@@ -0,0 +1,20 @@
+package pl.jasm.fcosb.seats;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "targetSeats")
+@Data
+public class TargetSeat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String targetSeatNo;
+
+    @ManyToOne
+    private Seat seat;
+}
---
+++ src/main/java/pl/jasm/fcosb/users/AppUser.java
@@ -0,0 +1,38 @@
+package pl.jasm.fcosb.users;
+
+import lombok.Data;
+import pl.jasm.fcosb.seats.Seat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 60)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private int enabled;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    @OneToOne
+    private Seat seat;
+}
